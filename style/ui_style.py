4536a4028930c2743efdb7ba940e7e33a2c46449
harigro <trierbank@gmail.com>
2024-06-13 13:44:21 +0700

    busana aplikasi

---
+++ style/ui_style.py
@@ -0,0 +1,27 @@
+stylesheet = """
+    QWidget {
+        background-color: #f0f0f0;
+        font-family: Arial, sans-serif;
+    }
+    QLabel {
+        color: #363636;
+    }
+    #lineEdit {
+        border: none;
+        color: #191919;
+    }
+    #lineEdit_2 {
+        border: none;
+        color: #787878;
+        font-size: 20px;
+    }
+    QPushButton {
+        background-color: #d9d9d9;
+        color: #191919;
+        border: none;
+        padding: 10px;
+    }
+    QPushButton:hover {
+        background-color: #bfbfbf;
+    }
+"""
